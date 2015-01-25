9c53fa57c5f8c2e16e8e6175da25bca1681cd7e0
lengie <cookiestork@gmail.com>
2015-01-26 01:16:31 +0700

    function now checks if matrix has changed

--- cachematrix.R
+++ cachematrix.R
@@ -2,33 +2,44 @@
 ## For 'R Programming' course in Jan 2015, programming assignment 2
 ## Written by LEngie, cookiestork@gmail.com
 
-## makeCacheMatrix will create a matrix obj that can cache its inverse
+## makeCacheMatrix will create a obj that can cache a matrix and its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-     m <- NULL #to keep any preexisting 'm' in the global from being redefined
+     inv <- NULL #to keep any preexisting 'inv' in the global from being redefined
      set <- function(y){
           x <<- y
-          m <<- NULL
+          inv <<- NULL #resets inverse if a previous matrix had been calculated
      }
-     get <- function() x 
-     setinverse <- function(solve) m <<- solve #this calls the function solve
-     getinverse <- function() m #this gets it from cache
-     list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
+     get <- function() x
+     setinverse <- function(inverse) inv <<- inverse #stores solve from cacheSolve
+     getinverse <- function() inv #this gets it from cache
+     
+     #a list output with functions for elements
+     list(orig=x,set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
 
 ## cacheSolve will compute the inverse of the matrix from makeCacheMatrix and retrieve
 ##  it from the cache
+## Inputs: f, makeCacheMatrix, and mat, the matrix to be solved
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-     m <- x$getinverse() #check to see if it is already cached
-     if(!is.null(m)) {
-          message("getting cached data")
-          return(m)
+cacheSolve <- function(f,...) {
+     ## Return a matrix that is the inverse of 'x', input to makeCacheMatrix
+     inv <- f$getinverse() 
+     
+     #check to see if it is already cached and if the matrix has changed
+     if(!is.null(inv)) {
+          #testing whether f$set has changed
+          ident <- identical(f$get(),f$orig)
+          if(ident==FALSE){
+               return("Matrix has changed. Rerun makeCacheMatrix with correct matrix")
+          }
+          message("Getting cached data")
+          return(inv) #will stop here if calculated for the first time
+     
      }
-     data <- x$get()
-     m <- solve(x)
-     x$setinverse(m)
-     m
+     data <- f$get()
+     inv <- solve(data)
+     f$setinverse(inv)
+     #inv
 }
